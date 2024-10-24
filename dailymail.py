7676c9a1b522da7630a2c2586a19432afd0995a3
jujcic <jan.ujcic@parsek.com>
2024-10-24 23:47:23 +0200

    Added all the base files that we will need based on the current information.

---
+++ dailymail.py
@@ -0,0 +1,27 @@
+
+
+
+# Google client info
+
+
+# Get the credentials
+
+
+def main():
+    
+    # STEP 1: get a list of existing files from the Drive
+
+    # STEP 2: grab the metadata files (recipients/sending_config & recently_sent_files)
+
+    # STEP 3: grab the data from the metadata files
+    recipients = []
+    nb_saved_old_files = 30
+    ignore_files = []
+
+    # STEP 4: Grab a random file from the existin files on the Drive (ignore if in ignore_files)
+
+    # STEP 5: Process the random file
+
+    # STEP 6: Send the file content via email
+
+    # STEP 7: Update the ignore_files folder and save it to the drive 
