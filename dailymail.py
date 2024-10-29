657a1e85e4518bddee42f06ff747b200355d8ab2
jujcic <jan.ujcic@parsek.com>
2024-10-29 22:24:20 +0100

    First test to authenticate with Google Drive and get back a list of files in a folder.

--- dailymail.py
+++ dailymail.py
@@ -1,15 +1,36 @@
+import configparser
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
 
-
+# Load configuration from config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 # Google client info
+SERVICE_ACCOUNT_FILE = config['google_drive']['service_account_file']
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+FOLDER_ID = config['google_drive']['folder_id']
 
+creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+drive_service = build('drive', 'v3', credentials=creds)
 
-# Get the credentials
+def list_files_in_folder(folder_id):
+    """List all files in a specified Google Drive folder."""
+    results = drive_service.files().list(
+        q=f"'{folder_id}' in parents",
+        spaces='drive',
+        fields="files(id, name)"
+    ).execute()
+    files = results.get('files', [])
+    for file in files:
+        print(f"File ID: {file['id']} - Name: {file['name']}")
+    return files
 
 
 def main():
     
     # STEP 1: get a list of existing files from the Drive
+    list_files_in_folder(FOLDER_ID)
 
     # STEP 2: grab the metadata files (recipients/sending_config & recently_sent_files)
 
@@ -25,3 +46,6 @@ def main():
     # STEP 6: Send the file content via email
 
     # STEP 7: Update the ignore_files folder and save it to the drive 
+
+if __name__ == '__main__':
+    main()
