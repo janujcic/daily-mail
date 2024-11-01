a4b3758833160b25f4590a61549850cce2147cff
jujcic <jan.ujcic@parsek.com>
2024-11-01 23:58:19 +0100

    Added a function to add the sent file to an array of files that were already sent. The updated json with the up-to-date list of recently sent items is then updated to the Google Drive.

--- dailymail.py
+++ dailymail.py
@@ -2,6 +2,7 @@ import configparser
 import json
 import smtplib
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaInMemoryUpload
 from google.oauth2.service_account import Credentials
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -24,8 +25,11 @@ def get_file_gdrive(drive_service, file_id):
     try:
         # Get the file metadata to determine the MIME type
         file_metadata = drive_service.files().get(fileId=file_id, fields='name, mimeType').execute()
-        file_name = file_metadata['name'].split('.')[0]
+        file_name = file_metadata['name']
+        file_title = file_metadata['name'].split('.')[0]
+        file_extension = file_metadata['name'].split('.')[1]
         mime_type = file_metadata['mimeType']
+        
         
         # Retrieve the file content
         request = drive_service.files().get_media(fileId=file_id)
@@ -36,7 +40,7 @@ def get_file_gdrive(drive_service, file_id):
             file_content = file_content.decode('utf-8')
         
         print(f"Successfully retrieved content of file: {file_name}")
-        file = {'file_name':file_name, 'mime_type':mime_type, 'file_content':file_content}
+        file = {'file_name':file_name, 'file_title': file_title, 'mime_type':mime_type, 'file_content':file_content, 'file_extension': file_extension, 'id':file_id}
         return file
     
     except Exception as e:
@@ -73,6 +77,65 @@ def send_email_smtp(gmail_username, gmail_password, recipient_emails, subject, m
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def update_recently_sent_file_json(sending_config, sent_file):
+    """ Updates the recently sent files part of the sending_config.json file"""
+
+    sending_config_content = json.loads(sending_config['file_content'])
+    recently_sent_files = sending_config_content['recently_sent_files']
+    file_number_limit = sending_config_content['file_number_limit']
+
+    if (len(recently_sent_files) > file_number_limit):
+        recently_sent_files.pop()
+    
+    if sent_file not in recently_sent_files:
+        recently_sent_files.append(sent_file)
+
+    sending_config_content['recently_sent_files'] = recently_sent_files
+
+    sending_config['file_content'] = sending_config_content
+
+    return sending_config
+
+def update_file_from_memory_to_gdrive(drive_service, folder_id, file):
+    """ Uploads a file to Google Drive from in-memory data. """
+    file_name = file['file_name']
+
+    file_metadata = {'name': file_name}
+
+    file_content = file['file_content']
+
+    if isinstance(file_content, str):
+        file_content = file_content.encode('utf-8')
+    elif isinstance(file_content, dict):
+        file_content = json.dumps(file_content).encode('utf-8')
+
+    media = MediaInMemoryUpload(file_content, mimetype=file['mime_type'], resumable=True)
+    print(file)
+    try:
+        if file['id']:
+             uploaded_file = drive_service.files().update(
+                fileId=file['id'],
+                body=file_metadata,
+                media_body=media,
+                fields='id, name, mimeType, parents'
+            ).execute()
+             print(f"Updated file '{file_name}' with ID: {uploaded_file['id']}")
+        else:
+            # Specify the folder to upload to, if provided
+            if folder_id:
+                file_metadata['parents'] = [folder_id]
+            # Upload the file
+            uploaded_file = drive_service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id, name, mimeType, parents'
+            ).execute()
+        
+            print(f"Uploaded file '{file_name}' with ID: {uploaded_file['id']}")
+    
+    except Exception as e:
+        print(f"An error occurred while uploading the file: {e}")
+
 def main():
     # Load configuration from config.ini
     config = configparser.ConfigParser()
@@ -93,18 +156,17 @@ def main():
     # STEP 1: get a list of existing files from the Drive
     files = list_files_gdrive_folder(drive_service, FOLDER_ID)
 
-    file = get_file_gdrive(drive_service, files[2]['id'])
+    file = get_file_gdrive(drive_service, files[3]['id'])
 
-    sending_config_metadata = find_file_from_list(files, sending_config_name)
-    sending_config_file = get_file_gdrive(drive_service, sending_config_metadata['id'])
+    sending_config_file = get_file_gdrive(drive_service, find_file_from_list(files, sending_config_name)['id'])
     sending_config = json.loads(sending_config_file["file_content"])
     recipients = sending_config['recipients']
+
+    new_sending_config_file = update_recently_sent_file_json(sending_config_file, file['file_name'])
 
     # STEP 2: grab the metadata files (recipients/sending_config & recently_sent_files)
 
     # STEP 3: grab the data from the metadata files
-    nb_saved_old_files = 30
-    ignore_files = []
 
     # STEP 4: Grab a random file from the existin files on the Drive (ignore if in ignore_files)
 
@@ -112,10 +174,12 @@ def main():
 
     # STEP 6: Send the file content via email
     if file:
-        subject = f"{file['file_name']}"
-        send_email_smtp(GMAIL_USERNAME, GMAIL_PASSWORD, recipients, subject, file['file_content'])
+        subject = f"{file['file_title']}"
+        #send_email_smtp(GMAIL_USERNAME, GMAIL_PASSWORD, recipients, subject, file['file_content'])
 
     # STEP 7: Update the ignore_files folder and save it to the drive 
+    update_file_from_memory_to_gdrive(drive_service, FOLDER_ID, new_sending_config_file)
+
 
 if __name__ == '__main__':
     main()
